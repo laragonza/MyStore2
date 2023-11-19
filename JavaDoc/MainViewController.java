9c647bf0db0d30de9ae8c2b6486bc60b79ddbbf9
lari <laragonza1294@gmail.com>
2023-11-19 19:40:26 +0100

    MainViewController.java

--- JavaDoc/MainViewController.java
+++ JavaDoc/MainViewController.java
@@ -1,5 +1,3 @@
-package com.example.mystore;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,70 +6,81 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import java.io.File;
 import java.net.URL;
-
-
 import java.io.IOException;
 import java.util.ResourceBundle;
 
+/**
+ * Controlador para la vista principal de la aplicación MyStore.
+ * Gestiona la reproducción de un video de bienvenida y la navegación a las vistas de inicio de sesión y registro.
+ */
 public class MainViewController implements Initializable {
+
     @FXML
     private Button btnMostrarInicioSesion, btnMostrarRegistro;
+    
     @FXML
     private TextField txtMostrar;
 
     @FXML
     private MediaView mediaView;
-    File file = new File("C:\\Users\\Raúl\\IdeaProjects\\MyStore\\src\\main\\resources\\Vid\\VideoPrincipal.mp4");
+    
     private Media media;
     private MediaPlayer mediaPlayer;
 
+    // Ruta del archivo de video
+    File file = new File("C:\\Users\\Raúl\\IdeaProjects\\MyStore\\src\\main\\resources\\Vid\\VideoPrincipal.mp4");
 
-    //Esto sive para añadir el link a las vistas
+    // Instancia de la aplicación principal para la navegación entre vistas
     HelloApplication main = new HelloApplication();
 
-    //Dirección de las diferentes vistas que se podrán usar
+    // Cargadores de vistas
     FXMLLoader loaderLogin = new FXMLLoader(getClass().getResource("LoginView.fxml"));
     FXMLLoader loaderRegistro = new FXMLLoader(getClass().getResource("RegistroView.fxml"));
 
+    /**
+     * Inicializa el controlador, carga el video y lo reproduce.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         media = new Media(file.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
-        // Botones de relleno
         mediaPlayer.play();
     }
 
+    /**
+     * Muestra la vista de inicio de sesión al hacer clic en el botón correspondiente.
+     *
+     * @param event El evento de acción que desencadena la acción.
+     */
     @FXML
     void MostrarViewInicioSesion(ActionEvent event) {
         try {
-            //creamos objeto del Main para poder llamar al metodo start2 /
             main.cerrarPagina(event, btnMostrarInicioSesion);
             main.mostrarPagina(event, loaderLogin);
-
-        } catch(Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Muestra la vista de registro al hacer clic en el botón correspondiente.
+     *
+     * @param event El evento de acción que desencadena la acción.
+     */
     @FXML
     void MostrarViewRegistro(ActionEvent event) {
         try {
-            // creamos objeto del Main para poder llamar al metodo start2 */
             main.cerrarPagina(event, btnMostrarRegistro);
             main.mostrarPagina(event, loaderRegistro);
-
-        } catch(Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
