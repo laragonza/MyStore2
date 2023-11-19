bd18f17f2b57dd388d8edff6d4cdaa33eaa764ba
lari <laragonza1294@gmail.com>
2023-11-19 19:41:48 +0100

    ProductDetailsViewController.java

--- JavaDoc/ProductDetailsViewController.java
+++ JavaDoc/ProductDetailsViewController.java
@@ -1,9 +1,7 @@
-// ProductDetailsViewController.java
-package com.example.mystore;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
+/**
+ * Controlador para la vista de detalles de producto en la aplicación MyStore.
+ * Muestra información detallada sobre un producto seleccionado.
+ */
 public class ProductDetailsViewController {
 
     @FXML
@@ -15,7 +13,11 @@ public class ProductDetailsViewController {
 
     private ProductViewController currentProduct;
 
-
+    /**
+     * Establece el producto actual y configura las etiquetas con su información.
+     *
+     * @param product El producto para el cual se mostrarán los detalles.
+     */
     public void setProduct(ProductViewController product) {
         this.currentProduct = product;
 
@@ -26,8 +28,12 @@ public class ProductDetailsViewController {
         // Puedes agregar más configuraciones según sea necesario
     }
 
+    /**
+     * Establece el producto actual y configura las etiquetas con su información.
+     *
+     * @param product El producto para el cual se mostrarán los detalles.
+     */
     public void setCurrentProduct(ProductViewController product) {
-        this.currentProduct = product;
         setProduct(product);
     }
 }
