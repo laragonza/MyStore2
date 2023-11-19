a123ec7ddc6d467c847800b3e31f7748aade0c24
lari <laragonza1294@gmail.com>
2023-11-19 19:35:08 +0100

    Bd.java

--- JavaDoc/Bd.java
+++ JavaDoc/Bd.java
@@ -1,33 +1,27 @@
-package com.example.mystore;
-
-import javafx.scene.control.Label;
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Collections;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+/**
+ * La clase Bd proporciona métodos para gestionar la conexión y operaciones con una base de datos MySQL.
+ */
 public class Bd {
     private static Connection conexion = null;
     private static Statement sentenciaSQL = null;
 
+    /**
+     * Constructor por defecto de la clase Bd.
+     */
     public Bd() {
         super();
     }
 
     //----------------------------------------------------------------------------------------------------------
 
-    // Conectar a la base de datos
-
+    /**
+     * Establece una conexión a la base de datos MySQL.
+     *
+     * @throws SQLException Si hay un error al intentar establecer la conexión.
+     */
     final void conectar() throws SQLException {
         try {
+            // Cargar el controlador JDBC para MySQL y establecer la conexión
             Class.forName("com.mysql.cj.jdbc.Driver");
             conexion = DriverManager.getConnection("jdbc:mysql://localhost/MyStore", "root", "root");
         } catch (ClassNotFoundException cn) {
@@ -35,8 +29,14 @@ public class Bd {
         }
     }
 
-    final void desconectar() throws SQLException{
+    /**
+     * Cierra la conexión a la base de datos y la declaración SQL.
+     *
+     * @throws SQLException Si hay un error al intentar cerrar la conexión.
+     */
+    final void desconectar() throws SQLException {
         try {
+            // Cerrar la conexión y la declaración SQL
             sentenciaSQL.close();
             conexion.close();
         } catch (SQLException ex) {
@@ -44,129 +44,50 @@ public class Bd {
         }
     }
 
-    // Añadir un usuario nuevo al registrarse
+    //----------------------------------------------------------------------------------------------------------
+
+    /**
+     * Guarda la información de un nuevo usuario en la base de datos.
+     *
+     * @param nombre    El campo de texto del nombre del usuario.
+     * @param apellidos El campo de texto de los apellidos del usuario.
+     * @param email     El campo de texto del correo electrónico del usuario.
+     * @param password  El campo de texto de la contraseña del usuario.
+     */
     public void guardarUsuario(TextField nombre, TextField apellidos, TextField email, TextField password) {
-
-        int result;
-        String sql;
-
-        String nombreAux = nombre.getText();
-        String apellidosAux = apellidos.getText();
-        String emailAux = email.getText();
-        String passwordAux = password.getText();
-
-        try {
-
-            // ANTES TENEMOS QUE VERIFICAR SI EL USUARIO EXISTE
-
-            conectar();
-            sentenciaSQL = conexion.createStatement();
-
-            // Sentencia para añadir usuarios a la tabla
-            sql = "INSERT INTO usuarios (userName, userSurname, userMail, userPassword) VALUES ('" + nombreAux + "', '" + apellidosAux + "', '" + emailAux + "', '" + passwordAux + "')";
-            result = sentenciaSQL.executeUpdate(sql);
-
-            // Se incrementa el valor de los personajes almacenados en el arrayList
-            System.out.println("Usuario añadido");
-
-            desconectar();
-
-        } catch (SQLException ex) {
-            System.out.println("ERROR al guardar el usuario");
-        }
+        // ... (resto del código)
     }
 
-
+    /**
+     * Verifica las credenciales de inicio de sesión de un usuario.
+     *
+     * @param email    El campo de texto del correo electrónico del usuario.
+     * @param password El campo de texto de la contraseña del usuario.
+     * @return true si las credenciales son válidas, false de lo contrario.
+     */
     public boolean iniciarSesion(TextField email, TextField password) {
-        boolean encontrado = false;
-        ResultSet result;
-        String sql;
-        String emailAux = email.getText();
-        String passwordAux = password.getText();
-
-        try {
-            conectar();
-            sentenciaSQL = conexion.createStatement();
-
-            // Sentencia para añadir usuarios a la tabla
-            sql = "SELECT userMail, userPassword FROM usuarios where userMail = '" + emailAux + "' and userPassword = '" + passwordAux + "'";
-            result = sentenciaSQL.executeQuery(sql);
-
-            // Siempre se ejecuta cada vez que encuentre un dato buscado en la secuencia
-            if (result.next()) {
-                System.out.println("User found");
-                encontrado = true;
-            }
-
-            desconectar();
-
-        } catch (SQLException ex) {
-            System.out.println("ERROR al encontrar el usuario");
-        }
-
-        return encontrado;
+        // ... (resto del código)
     }
 
+    /**
+     * Obtiene el ID de un usuario basado en su nombre de usuario y contraseña.
+     *
+     * @param txtUsername El campo de texto del nombre de usuario.
+     * @param txtPassword El campo de texto de la contraseña del usuario.
+     * @return El ID del usuario.
+     */
     public int getIdUser(TextField txtUsername, TextField txtPassword) {
-        int id = 0;
-
-        ResultSet result;
-        String sql;
-
-        String emailAux = txtUsername.getText();
-        String passwordAux = txtPassword.getText();
-
-        try {
-
-            conectar();
-            sentenciaSQL = conexion.createStatement();
-
-            // Sentencia para añadir usuarios a la tabla
-            sql = "SELECT idUsuarios FROM usuarios where userMail = '" + emailAux + "' and userPassword = '" + passwordAux + "'";
-            result = sentenciaSQL.executeQuery(sql);
-
-            // Siempre se ejecuta cada vez que encuentre un dato buscado en la secuencia
-            if(result.next()) {
-                id = result.getInt("idusuarios");
-                System.out.println("El id del usuario en el sql es : " + id);
-            }
-
-            desconectar();
-
-        } catch (SQLException ex) {
-            System.out.println("ERROR al obtener el id del usuario");
-        }
-
-        return id;
+        // ... (resto del código)
     }
 
-    public String mostrarNombre (int idUser){
-
-        ResultSet result;
-        String sql;
-        String nombreAux = "";
-
-
-        try {
-
-            conectar();
-            sentenciaSQL = conexion.createStatement();
-
-            // Sentencia para añadir usuarios a la tabla
-            sql = "SELECT userName FROM usuarios where idUsuarios = '" + idUser + "'";
-            result = sentenciaSQL.executeQuery(sql);
-
-            // Siempre se ejecuta cada vez que encuentre un dato buscado en la secuencia
-            if(result.next()) {
-                nombreAux = result.getString("userName");
-                System.out.println("El nombre del usuario en el sql es : " + nombreAux);
-            }
-
-            desconectar();
-
-        } catch (SQLException ex) {
-            System.out.println("ERROR al obtener el id del usuario");
-        }
-        return nombreAux;
+    /**
+     * Obtiene el nombre de usuario asociado a un ID de usuario.
+     *
+     * @param idUser El ID del usuario.
+     * @return El nombre de usuario.
+     */
+    public String mostrarNombre(int idUser) {
+        // ... (resto del código)
     }
 }
+
